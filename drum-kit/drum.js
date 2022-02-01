5fd2ada6e9eb974cde90fc3730fd5c578ca91a59
Seth <sepx898508@gmail.com>
2022-02-01 23:34:54 +0800

    feat: finish drum

--- drum-kit/drum.js
+++ drum-kit/drum.js
@@ -1,16 +1,23 @@
 window.addEventListener('DOMContentLoaded', () => {
 
-  window.addEventListener('keydown', (e) => {
+  function playSound(e) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
 
-    if(!audio) return;
-
+    if (!audio) return;
+    audio.currentTime = 0;
     audio.play();
 
-    console.log(audio);
-    // console.log(e.keyCode);
-  });
+    key.classList.add("playing");
+  }
 
-  console.log("Hi");
+  function removeTransition(e) {
+    if (e.propertyName !== 'transform') return;
+    this.classList.remove("playing");
+  }
 
+  const keys = document.querySelectorAll(".key");
+  keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+  window.addEventListener('keydown', playSound);
 });
